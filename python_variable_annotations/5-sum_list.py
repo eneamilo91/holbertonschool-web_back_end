d1e8ccc8f5968352b206cdecbf413352af36e6f6
eneamilo91 <eneamilo514@gmail.com>
2024-04-22 18:24:23 +0000

    added several files

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" module of a function"""
+from typing import List
+
+def sum_list(input_list :float) -> float:
+    "Func to return a sum"
+    sum = 0
+    for elem in input_list:
+        sum += elem
+    return float(sum)
